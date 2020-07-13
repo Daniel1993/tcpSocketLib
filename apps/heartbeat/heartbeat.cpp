260ef2479ac2f8587b43cf6d5ae59f592d7c4c42
dcastro <daniel.castro@ist.utl.pt>
2020-07-13 15:10:16 +0100

    keyManager working, heartbeat still work in progress

--- apps/heartbeat/heartbeat.cpp
+++ apps/heartbeat/heartbeat.cpp
@@ -24,6 +24,7 @@ static map<string,int> secrets;
 static map<string, tuple<string, string, RemoteEntity, int, int>> otherNodes;
 Entity *myId;
 tsl_identity_t *caCert;
+Server *server;
 
 typedef enum { HEARTBEAT = 1, ACK, NACK, KEY_XCH } MSG_TYPE;
 
@@ -44,15 +45,6 @@ typedef struct {
   size_t signatureSize;
 } envelope_s;
 
-static int cmpHmacs(unsigned char *hmac1, unsigned char *hmac2)
-{
-  // there is a AVX512 instruction that can compare 64 Bytes 
-  for (int i = 0; i < TSL_HASH_SIZE; ++i) {
-    if (hmac1[i] != hmac2[i]) return 0;
-  }
-  return 1;
-}
-
 int handle_nodes(const char *inputArg)
 {
   char buffer[1024];
@@ -61,7 +53,7 @@ int handle_nodes(const char *inputArg)
   if (arg.rfind("node", 0) == 0) {
     if (input_getString((char *)inputArg, buffer) > 1024) {
       printf("buffer overflow!\n");
-      return 0;
+      return 1;
     }
     string value(buffer);
     string addr, port, publKey;
@@ -77,11 +69,10 @@ int handle_nodes(const char *inputArg)
     if (value.length() == 0) return 0;
     publKey = value;
 
-    Entity entity(arg, addr, atoi(port.c_str()));
+    RemoteEntity entity(arg, addr, atoi(port.c_str()));
 
-    entity.SetLocalKeys(publKey);
     entity.LoadId();
-    if (!entity.VerifyId(masterCert)) {
+    if (!entity.VerifyId(caCert)) {
       printf("[ERROR]: verification of %s cert failed\n", arg.c_str());
     }
     
@@ -92,38 +83,35 @@ int handle_nodes(const char *inputArg)
   return 0;
 }
 
-void exchangeKeyCallback(Message msg)
+int exchangeKeyCallback(Message &msg)
 {
   envelope_s response;
   long size;
   envelope_s *castMsg = (envelope_s*)msg.GetContents(size);
-  unsigned char hmac[TSL_HASH_SIZE];
   char responseOk[] = "ok";
   char responseNOk[] = "not-ok";
-  int cmp = 0, verify = 0;
   RemoteEntity otherId;
+  RemoteEntity otherKey;
 
   if (size != sizeof(envelope_s)) {
     printf("error, message size mismatch (got %zu B, expected %zu B)\n", sizeof(envelope_s), size);
     Message resp((unsigned char*)responseNOk, (size_t)sizeof(responseNOk));
     msg.RespondWith(resp);
-    return;
+    return 1;
   }
 
   // exchange key with node
   string otherNode = string(castMsg->msg.from);
   otherId = get<2>(otherNodes[otherNode]);
 
-  verify = otherId.VerifyMsg(msg);
-  if (!verify) {
+  if (!otherId.VerifyMsg(msg)) {
     printf("[ERROR]: signature failed!\n"); // TODO: abort program
   }
 
   auto tuple = otherNodes[otherNode];
   otherNodes[string(castMsg->msg.from)] = make_tuple(get<0>(tuple), get<1>(tuple), get<2>(tuple), 1, get<4>(tuple));
 
-  RemoteEntity otherKey();
-  otherId.DeserlKey(castMsg->msg.text, 2048);
+  otherKey.DeserlKey(castMsg->msg.text, 2048);
   otherId.PairWithKey(otherKey.GetId());
 
   memcpy(response.msg.text, responseOk, strlen(responseOk));
@@ -135,99 +123,52 @@ void exchangeKeyCallback(Message msg)
 
   printf("[XCH]: got pub_key from %s!\n", otherNode.c_str());
   // end of manage key
+
+  return 0;
 }
 
-void heartbeatCallback(Message msg)
+int heartbeatCallback(Message &msg)
 {
   // normal heartbeat
+  long size;
   envelope_s *castMsg = (envelope_s *)msg.GetContents(size);
   string otherNode = string(castMsg->msg.from);
-  otherId = get<2>(otherNodes[otherNode]);
-    if (!get<3>(otherNodes[otherNode])) return;
-  // tsl_load_publkey((char*)get<3>(otherNodes[string(castMsg->msg.text)]).c_str());
-  tsl_id_hmac(otherId, &(castMsg->msg), sizeof(msg_s), hmac, &size);
-  verify = tsl_id_verify(otherId, &(castMsg->signature),
-    castMsg->signatureSize, &(castMsg->msg), sizeof(msg_s));
-  // tsl_verify(&(castMsg->signature), castMsg->signatureSize, &(castMsg->msg), sizeof(msg_s));
-  cmp = cmpHmacs((unsigned char*)castMsg->hmac, (unsigned char*)hmac);
-  printf("send_heartbeat_to \"%s\" (type = %i, hmac = %i, signature = %i)\n",
-    castMsg->msg.from, castMsg->msg.header.type, cmp, verify);
-  if (cmp) {
-    memcpy(response.msg.text, responseOk, strlen(responseOk));
-    response.msg.header.type = ACK;
-  } else {
-    memcpy(response.msg.text, responseNOk, strlen(responseNOk));
-    response.msg.header.type = NACK;
-  }
-  // end of heartbeat
-}
-
-void heartbeat(void *msg, size_t len, void(*respondWith)(void*,size_t), void(*waitResponse)(void*, size_t*))
-{
-  envelope_s response;
-  envelope_s *castMsg = (envelope_s*)msg;
-  unsigned char hmac[TSL_HASH_SIZE];
-  size_t size;
+  RemoteEntity otherId = get<2>(otherNodes[otherNode]);
   char responseOk[] = "ok";
   char responseNOk[] = "not-ok";
-  int cmp = 0, verify = 0;
-  tsl_identity_t *otherId;
+  envelope_s response;
+  if (!get<3>(otherNodes[otherNode])) return 1;
 
-  if (len != sizeof(envelope_s)) {
-    printf("error, message size mismatch (got %zu B, expected %zu B)\n", sizeof(envelope_s), len);
-    respondWith(responseNOk, sizeof(responseNOk));
-    return;
-  }
-
-  memset(&response, 0, sizeof(response));
-  if (castMsg->msg.header.type == KEY_XCH)
-  {
-    // exchange key with node
-    string otherNode = string(castMsg->msg.from);
-    otherId = get<2>(otherNodes[otherNode]);
-
-    verify = tsl_id_verify(otherId, &(castMsg->signature),
-      castMsg->signatureSize, &(castMsg->msg), sizeof(msg_s));
-    if (!verify) {
-      printf("[ERROR]: signature failed!\n"); // TODO: abort program
-    }
-
-    auto tuple = otherNodes[otherNode];
-    otherNodes[string(castMsg->msg.from)] = make_tuple(get<0>(tuple), get<1>(tuple), get<2>(tuple), 1, get<4>(tuple));
-
-    tsl_id_deserialize_ec_pubkey(otherId, castMsg->msg.text, 2048);
-    tsl_id_gen_peer_secret(myId, otherId);
-    tsl_id_load_secret(otherId, NULL);
-
+  // tsl_load_publkey((char*)get<3>(otherNodes[string(castMsg->msg.text)]).c_str());
+  
+  // msg.Verify(otherId.GetId()); // TODO: change between HMAC/Signature
+  if (msg.HmacVerify(otherId.GetId())) {
+    printf("HMAC verify success!\n");
+    
     memcpy(response.msg.text, responseOk, strlen(responseOk));
     response.msg.header.type = ACK;
-    printf("[XCH]: got pub_key from %s!\n", otherNode.c_str());
-    // end of manage key
+    Message resp((unsigned char*)&response, sizeof(envelope_s));
+    msg.RespondWith(resp);
+
+  } else if (msg.Verify(otherId.GetId())) {
+    printf("Signature verify success (no HMAC)!\n");
+    
+    memcpy(response.msg.text, responseOk, strlen(responseOk));
+    response.msg.header.type = ACK;
+    Message resp((unsigned char*)&response, sizeof(envelope_s));
+    msg.RespondWith(resp);
+
+  } else {
+    printf("HMAC && Signature verify fail!\n");
+
+    memcpy(response.msg.text, responseNOk, strlen(responseNOk));
+    response.msg.header.type = NACK;
+    Message resp((unsigned char*)&response, sizeof(envelope_s));
+    msg.RespondWith(resp);
+
   }
-  else
-  {
-    // normal heartbeat
-    string otherNode = string(castMsg->msg.from);
-    otherId = get<2>(otherNodes[otherNode]);
-     if (!get<3>(otherNodes[otherNode])) return;
-    // tsl_load_publkey((char*)get<3>(otherNodes[string(castMsg->msg.text)]).c_str());
-    tsl_id_hmac(otherId, &(castMsg->msg), sizeof(msg_s), hmac, &size);
-    verify = tsl_id_verify(otherId, &(castMsg->signature),
-      castMsg->signatureSize, &(castMsg->msg), sizeof(msg_s));
-    // tsl_verify(&(castMsg->signature), castMsg->signatureSize, &(castMsg->msg), sizeof(msg_s));
-    cmp = cmpHmacs((unsigned char*)castMsg->hmac, (unsigned char*)hmac);
-    printf("send_heartbeat_to \"%s\" (type = %i, hmac = %i, signature = %i)\n",
-      castMsg->msg.from, castMsg->msg.header.type, cmp, verify);
-    if (cmp) {
-      memcpy(response.msg.text, responseOk, strlen(responseOk));
-      response.msg.header.type = ACK;
-    } else {
-      memcpy(response.msg.text, responseNOk, strlen(responseNOk));
-      response.msg.header.type = NACK;
-    }
-    // end of heartbeat
-  }
-  respondWith((void*)&response, sizeof(response));
+  // end of heartbeat
+  return 0;
 }
 
 void sigIntHandler_fn(int s){
@@ -274,11 +215,11 @@ void timer_handler(void *arg)
       memcpy(env.msg.text, exchangeKey, 2048);
       env.msg.header.type = KEY_XCH;
       env.signatureSize = TSL_SIGNATURE_SIZE;
-      tsl_id_sign(myId, &(env.msg), sizeof(msg_s), env.signature, &(env.signatureSize));
-      if (env.signatureSize > TSL_SIGNATURE_SIZE) printf("signature buffer overflow!\n");
-      tsl_send_msg(connId, (void*)&env, sizeof(envelope_s));
 
+      Message msg((unsigned char*)&env, sizeof(envelope_s));
 
+      msg.Sign(myId->GetId());
+      server->SendMsg(msg);
       // clock_gettime(CLOCK_MONOTONIC_RAW, &start);
       // tsl_recv_msg(connId, &respOk, &size); // TODO: check message
       // clock_gettime(CLOCK_MONOTONIC_RAW, &end);
@@ -292,23 +233,20 @@ void timer_handler(void *arg)
       memcpy(env.msg.from, id, sizeof(id));
       memcpy(env.msg.text, "ping", sizeof(id));
       env.msg.header.type = HEARTBEAT;
-      tsl_identity_t *otherId = get<2>(otherNodes[it->first]);
-      tsl_id_hmac(otherId, &(env.msg), sizeof(msg_s), env.hmac, &size);
-      if (size > TSL_HASH_SIZE) {
-        printf("hmac buffer overflow (%s, size=%lu)!\n", tsl_last_error_msg, size);
-      }
-      env.signatureSize = TSL_SIGNATURE_SIZE;
-      tsl_id_sign(myId, &(env.msg), sizeof(msg_s), env.signature, &(env.signatureSize));
-      if (env.signatureSize > TSL_SIGNATURE_SIZE) printf("signature buffer overflow!\n");
+      RemoteEntity otherId = get<2>(otherNodes[it->first]);
+      Message msg((unsigned char*)&env, sizeof(envelope_s));
+
+      msg.Sign(myId->GetId());
+      server->SendMsg(msg);
 
       // printf("connected (%i)!\n", connId);
-      clock_gettime(CLOCK_MONOTONIC_RAW, &start);
-      tsl_send_msg(connId, (void*)&env, sizeof(envelope_s));
-      tsl_recv_msg(connId, &respOk, &size); // TODO: check message
-      clock_gettime(CLOCK_MONOTONIC_RAW, &end);
-      durationMs = ((end.tv_sec * 1e9 + end.tv_nsec) - (start.tv_sec * 1e9 + start.tv_nsec)) / 1e6f;
-      printf("[%s] response from %s = \"%s\" (type = %i, lat = %.3fms)\n", id, it->first.c_str(),
-        respOk.msg.text, respOk.msg.header.type, durationMs);
+      // clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+      // tsl_send_msg(connId, (void*)&env, sizeof(envelope_s));
+      // tsl_recv_msg(connId, &respOk, &size); // TODO: check message
+      // clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+      // durationMs = ((end.tv_sec * 1e9 + end.tv_nsec) - (start.tv_sec * 1e9 + start.tv_nsec)) / 1e6f;
+      // printf("[%s] response from %s = \"%s\" (type = %i, lat = %.3fms)\n", id, it->first.c_str(),
+      //   respOk.msg.text, respOk.msg.header.type, durationMs);
     }
 
   }
@@ -341,36 +279,28 @@ int main(int argc, char **argv)
   }
 
   myId = new Entity();
-  myId->SetLocalKeys(privKey);
   myId->LoadId();
 
-  Server server(myId, atoi(port));
+  server = new Server(myId, atoi(port));
 
-  server.AddCallback(0, exchangeKeyCallback);
-  server.AddCallback(1, heartbeatCallback);
+  server->AddCallback(0, exchangeKeyCallback);
+  server->AddCallback(1, heartbeatCallback);
 
-  if (tsl_load_identity(myId, privKey, NULL, NULL, NULL, NULL)) {
-    printf("Error: %s\n", tsl_last_error_msg);
-    return EXIT_FAILURE;
-  }
   caCert = tsl_alloc_identity();
   if (tsl_load_identity(caCert, NULL, NULL, NULL, masterCert, NULL)) {
     printf("Error: %s\n", tsl_last_error_msg);
     return EXIT_FAILURE;
   }
-  if (!tsl_id_cert_verify(myId, caCert, NULL)) {
+  if (!myId->VerifyId(caCert)) {
     printf("ERROR, master did not certify my key!\n");
   }
   // TODO: send a generated key on first contact
-  tsl_id_gen_ec_key(myId);
-  tsl_id_serialize_ec_pubkey(myId, exchangeKey, 2048);
+  size_t size = 2048;
+  myId->SerlKey(exchangeKey, &size);
   printf("Gen key : \n%s", exchangeKey);
   input_foreach(handle_nodes); // do this only after the previous 3
 
   printf("%s listening on port %s\n", id, port);
-
-  tsl_init(port);
-  tsl_add_handler(heartbeat);
 
   sigIntHandler.sa_handler = sigIntHandler_fn;
   sigemptyset(&sigIntHandler.sa_mask);
